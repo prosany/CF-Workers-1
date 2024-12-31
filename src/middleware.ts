39e35142f6425efd45e6b202f9a77ad50eeaab02
sany <mahabubsany2@gmail.com>
2025-01-01 01:38:33 +0600

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   console.log('REQ:', request.url);
-  return NextResponse.redirect(new URL('https://github.com/prosany'));
+  // return NextResponse.redirect(new URL('https://github.com/prosany'));
 
   return NextResponse.next();
 }
